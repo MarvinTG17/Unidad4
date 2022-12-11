b2c528e01dee7518e1cf42a73043634dc279d8f9
Marvin <taipe.marvin17@gmail.com>
2022-12-10 21:56:14 -0500

    agregando host en allowed_hosts - settings

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!sq3wixjk#4*0#zi$cu4%6a#ytuu4k1ahk!=qu=*w(@w)yc66)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['unidad4-production.up.railway.app']
 
 
 # Application definition
