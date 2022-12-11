9012b64fa45595df3bdd0e647cfa83a09d737977
Marvin <taipe.marvin17@gmail.com>
2022-12-11 11:13:33 -0500

    settings-css

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 #from django.contrib.messages import constants as mensajes
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'proyectos'
 ]
 
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'portafolio.urls'
@@ -126,8 +128,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = BASE_DIR / "archivos"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = "/archivos/"
-
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [
     'proyectos/static/'
 ]
