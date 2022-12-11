dcdf1a6d1343f46eb725e194f2ce8ecf1e0a352b
Marvin <taipe.marvin17@gmail.com>
2022-12-11 11:26:07 -0500

    settings-css

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -131,9 +131,9 @@ MEDIA_ROOT = BASE_DIR / "archivos"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = "/archivos/"
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATICFILES_DIRS = [
+""" STATICFILES_DIRS = [
     'proyectos/static/'
-]
+] """
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
